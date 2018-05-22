cb5c953e1a78cd58e44dd63fdb034d9014d8185b
Joel Wu <joel.wu@oocl.com>
2018-05-22 15:34:59 +0800

    FizzBuzz

--- src/main/java/com/example/demo/controller/FizzBuzzController.java
+++ src/main/java/com/example/demo/controller/FizzBuzzController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FizzBuzzController {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "FizzBuzz" ;
+
+    public String comparator(int num) {
+        if (num%3 == 0 && num%5 !=0) {
+            return FIZZ;
+        } else if (num%3 !=0 &&num%5 ==0) {
+            return BUZZ;
+        }else if (num%3 == 0 && num%5 ==0) {
+            return FIZZBUZZ;
+        } else if (num < 0) {
+            throw new RuntimeException("input error");
+        }  else {
+            return String.valueOf(num);
+        }
+    }
 }
--- src/test/java/com/example/demo/controller/FizzBuzzControllerTest.java
+++ src/test/java/com/example/demo/controller/FizzBuzzControllerTest.java
@@ -1,5 +1,58 @@
+package com.example.demo.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzControllerTest {
+    FizzBuzzController fizzBuzzController;
+
+    @Before
+    public void init() {
+        fizzBuzzController = new FizzBuzzController();
+    }
+
+    @Test
+    public void shouldReturnFizzWhenGiven3 () {
+        int sourceNum = 3;
+        String targetResult = fizzBuzzController.comparator (sourceNum);
+        assertEquals("Fizz",targetResult);
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenGiven5 () {
+        int sourceNum = 5;
+        String targetResult = fizzBuzzController.comparator (sourceNum);
+        assertEquals("Buzz",targetResult);
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenGiven15 () {
+        int sourceNum = 15;
+        String targetResult = fizzBuzzController.comparator (sourceNum);
+        assertEquals("FizzBuzz",targetResult);
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenGiven4 () {
+        int sourceNum = 4;
+        String targetResult = fizzBuzzController.comparator (sourceNum);
+        assertEquals(String.valueOf(sourceNum),targetResult);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldReturnBuzzWhenGivenNegativeOne () {
+        int sourceNum = -1;
+        String targetResult = fizzBuzzController.comparator (sourceNum);
+//        assertEquals(String.valueOf(sourceNum),targetResult);
+    }
+
+
+
+
+
+
 
 }
